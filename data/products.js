c74dcab5c4e78692ac7a8139b40bc5eb28fda993
pratiyankkumar <kumarpratiyank@gmail.com>
2024-07-13 14:22:36 +0530

    Learn more details about classes and 'this'

--- data/products.js
+++ data/products.js
@@ -55,6 +55,31 @@ class Clothing extends Product{
   }
 }
 
+// const date = new Date();
+// console.log(date)
+// console.log(date.toLocaleTimeString());
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a
+// };
+
+// function logThis() {
+//   console.log(this);
+// }
+// logThis();
+// logThis.call('hello');
+
+// const object3 = {
+//   method: () => {
+//     console.log(this);
+//   }
+// };
+
+// object3.method();
+
 
 export const products = [
   {
