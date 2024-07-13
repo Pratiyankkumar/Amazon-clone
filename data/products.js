95ac7c95925aa0d347771366efb8931d99ae316f
pratiyankkumar <kumarpratiyank@gmail.com>
2024-07-13 14:05:01 +0530

    Use inheritance and polymorphism to ad size chart

--- data/products.js
+++ data/products.js
@@ -34,7 +34,27 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
 
 export const products = [
   {
@@ -696,5 +716,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
